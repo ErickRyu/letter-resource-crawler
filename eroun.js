b70882e56a709fde76e74847a9fbe0e67e640282
류성진 <sj.ryu@slowalk.co.kr>
2018-07-16 12:49:30 +0900

    Update: Append to google sheet

--- eroun.js
+++ eroun.js
@@ -1,12 +1,17 @@
 const cheerio = require('cheerio')
 const fetch = require('./fetch.js')
 const isDiffOneDays = require('./isDiffOneDays')
+const append = require('../googleAlertCrawler/index.js')
 
 const DATE_FORMAT = 'YYYY-MM-DD'
 
 
 var process = function (url) {
-    var devs = [];
+    var devs = [
+        [''],
+        ['이로운 넷'],
+        ['title', 'date', 'link']
+    ];
     fetch(url)
         .then(function (body) {
             $ = cheerio.load(body);
@@ -15,17 +20,19 @@ var process = function (url) {
         })
         .then(function (rows) {
             rows.each(function () {
-                var dev = {
-                    date: $(this).find('time').attr('datetime'),
-                    title: $(this).find('a').text().trim(),
-                    href: $(this).find('a').attr('href')
-                };
-                if(isDiffOneDays(dev.date , DATE_FORMAT))
+                var dev = [
+                    $(this).find('a').text().trim(), //title
+                    $(this).find('time').attr('datetime'), //date
+                    $(this).find('a').attr('href') //link
+                ];
+                if(isDiffOneDays(dev[1] , DATE_FORMAT))
                     devs.push(dev);
             });
         })
         .then(function () {
+            devs.push([''])
             console.log(devs)
+            append(devs)
         })
 }
 
