16da76af789086a31142be8de60d88ba58d100a6
류성진 <sj.ryu@slowalk.co.kr>
2018-07-23 17:54:02 +0900

    Extract findAndAppendElems() in futurechosun

--- futurechosun.js
+++ futurechosun.js
@@ -11,34 +11,35 @@ const name = '더 나은 미래'
 const url = 'http://futurechosun.com/all'
 const articleListSelector = '.grid > div'
 
+var devs = []
 var process = function (url) {
-    var devs = [
-        [''],
-        [name],
-    ];
-    fetch(url)
-        .then(function (body) {
-            $ = cheerio.load(body);
-            return $();
-        })
-        .then(function (rows) {
-            rows.each(function () {
-                var dev = [
-                    $(this).find('.title').eq(0).text().trim(), //title
-                    $(this).find('.date').eq(0).text().trim(), //date
-                    $(this).find('a').attr('href') //link
-                ];
-                if(isDiffOneDays(dev[1], DATE_FORMAT))
-                    devs.push(dev);
-            });
-        })
-        .then(function () {
-            devs.push([''])
-            console.log(devs)
-            append(devs)
-        })
+  devs = [
+    [''],
+    [name],
+  ];
+  fetch(url)
+    .then(function (body) {
+      $ = cheerio.load(body);
+      return $(articleListSelector);
+    })
+    .then(function (rows) {
+      rows.each(findAndAppendElems);
+    })
+    .then(function () {
+      devs.push([''])
+      console.log(devs)
+      append(devs)
+    })
 }
 
-
+function findAndAppendElems() {
+  var dev = [
+    $(this).find('.title').eq(0).text().trim(), //title
+    $(this).find('.date').eq(0).text().trim(), //date
+    $(this).find('a').attr('href') //link
+  ];
+  if(isDiffOneDays(dev[1], DATE_FORMAT))
+    devs.push(dev);
+}
 process(url)
 
